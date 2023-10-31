76cb4fb8b79da286516b5500b319146b8d0cc9d8
pablosanchezizquierdo <sanizbe.pablo@gmail.com>
2023-10-31 11:32:45 +0100

    test

--- R/app_server.R
+++ R/app_server.R
@@ -5,7 +5,7 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  mod_Plotting_server("Plotting_1")
   mod_DNAGenerator_server("DNAGenerator_1")
+  mod_Plotting_server("Plotting_1")
 }
 
--- R/mod_DNAGenerator.R
+++ R/mod_DNAGenerator.R
@@ -11,8 +11,8 @@ mod_DNAGenerator_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      column(8, shiny::uiOutput(ns("DNA"))),
-      column(4, shiny::numericInput(
+      column(8, uiOutput(ns("DNA"))),
+      column(4, numericInput(
         inputId = ns("dna_length"),
         value = 6000,
         min = 3,
@@ -20,19 +20,19 @@ mod_DNAGenerator_ui <- function(id){
         step = 3,
         label = "Random DNA length"
       ),
-      shiny::actionButton(
+      actionButton(
         inputId = ns("generate_dna"),
         label = "Generate random DNA", style = "margin-top: 18px;"
       ))
     ),
-    shiny::verbatimTextOutput(outputId = ns("peptide")) |>
-      shiny::tagAppendAttributes(style = "white-space: pre-wrap;")
+    verbatimTextOutput(outputId = ns("peptide")) |>
+      tagAppendAttributes(style = "white-space: pre-wrap;")
 
   )
 }
 
 #' DNAGenerator Server Functions
-#'
+#' @import CentralDogmaMD
 #' @noRd
 mod_DNAGenerator_server <- function(id){
   moduleServer( id, function(input, output, session){
@@ -52,6 +52,20 @@ mod_DNAGenerator_server <- function(id){
       dna(
         CentralDogmaMD::SampleDNA(input$dna_length)
       )
+    })
+    output$peptide <- renderText({
+      # Ensure input is not NULL and is longer than 2 characters
+      if(is.null(input$DNA)){
+        NULL
+      } else if(nchar(input$DNA) < 3){
+        NULL
+      } else{
+        input$DNA |>
+          toupper() |>
+          CentralDogmaMD::TranscriptionDNA() |>
+          CentralDogmaMD::CodonExtraction() |>
+          CentralDogmaMD::ConcatenateCodons()
+      }
     })
   })
 }
