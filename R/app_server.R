cfceb1adddd2b0baba70eac58b95d7691c9ce1fb
JavierLopezBort <s223237@student.dtu.dk>
2023-11-02 14:08:23 +0000

    change

--- R/app_server.R
+++ R/app_server.R
@@ -5,7 +5,7 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  mod_DNAGenerator_server("DNAGenerator")
+  mod_DNAGenerator_server("DNAGenerator_1")
   mod_Plotting_server("Plotting_1")
 }
 
